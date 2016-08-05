abc75cb057f7ce36716b2c7978002dc20c0bae82
William Mckee <will@artcontrol.me>
2016-08-06 01:03:52 +1200

    switch to nikola

--- gifey.py
+++ gifey.py
@@ -20,7 +20,7 @@
 import os
 
 
-# In[6]:
+# In[2]:
 
 import requests
 import json
@@ -33,42 +33,37 @@ import getpass
 from walkdir import filtered_walk, dir_paths, all_paths, file_paths
 
 
-# In[7]:
+# In[3]:
 
 getusr = getpass.getuser()
 
 
-# In[8]:
+# In[4]:
 
 getusr
 
 
-# In[3]:
+# In[17]:
 
 keyword = input('Keyword: ')
 
 
-# In[4]:
+# In[18]:
 
-from dominate.tags import *
-
-
-# In[11]:
-
-gifdi = os.listdir('/home/' + getusr + '/gify/')
+gifydir = ('/home/{}/gify/'.format(getusr))
 
 
-# In[12]:
+# In[19]:
 
-gifdi
-
-
-# In[13]:
-
-keywddir = ('/home/' + getusr + '/gify/' + keyword)
+gifdi = os.listdir('/home/{}/gify/posts'.format(getusr))
 
 
-# In[14]:
+# In[20]:
+
+keywddir = ('/home/{}/gify/galleries/{}'.format(getusr, keyword))
+
+
+# In[21]:
 
 if os.path.isdir(keywddir) == True:
     print ('its true')
@@ -77,32 +72,32 @@ else:
     os.mkdir(keywddir)
 
 
-# In[15]:
+# In[22]:
 
-opwritj = requests.get('http://api.giphy.com/v1/gifs/search?q=' + keyword + '&api_key=dc6zaTOxFJmzC')
+opwritj = requests.get('http://api.giphy.com/v1/gifs/search?q={}&api_key=dc6zaTOxFJmzC'.format(keyword))
 
 
-# In[16]:
+# In[23]:
 
 wrijrd = opwritj.text
 
 
-# In[17]:
+# In[24]:
 
 jswri = json.loads(wrijrd)
 
 
-# In[18]:
+# In[25]:
 
 jswln = len(jswri['data'])
 
 
-# In[19]:
+# In[26]:
 
 jswln
 
 
-# In[20]:
+# In[27]:
 
 for jsw in range(0, jswln):
     if '.gif' in jswri['data'][jsw]['images']['downsized']['url']:
@@ -113,102 +108,17 @@ for jsw in range(0, jswln):
             del response
 
 
-# In[23]:
-
-gifdi = os.listdir('/home/' + getusr + '/gify/')
-
-
-# In[24]:
-
-gifdi
-
-
-# In[25]:
-
-keyword
-
-
-# In[27]:
-
-posfild = ('/home/' + getusr + '/gify/site/posts/')
-
-
 # In[28]:
 
-posfild
+os.chdir(gifydir)
 
 
 # In[29]:
 
-#galdir = ('galleries/' + keyword + '/' +)
+os.system('nikola build')
 
 
-# In[44]:
-
-keyosli = os.listdir(keywddir)
+# In[ ]:
 
 
-# In[49]:
-
-doc = dominate.document(title='gify')
-
-with doc.head:
-    link(rel='stylesheet', href='style.css')
-    script(type ='text/javascript', src='script.js')
-    #str(str2)
-    
-    with div():
-        attr(cls='header')
-        h1('Gify ' + keyword)
-        p(img('imgs/logo.svg', src='imgs/logo.svg'))
-        #p(img('imgs/15/01/02/ReptileLover82-reference.png', src= 'imgs/15/01/02/ReptileLover82-reference.png'))
-        h1('Updated ', strftime("%a, %d %b %Y %H:%M:%S +0000", gmtime()))
-        #p(panz)
-    
-    
-
-with doc:
-    with div(id='body').add(ol()):
-        for rdz in keyosli:
-            #h1(rdz.title)
-            #a(rdz.url)
-            #p(img(rdz, src='%s' % rdz))
-            #print rdz
-            
-            p(img(rdz, src = rdz))
-            p(rdz)
-
-
-                
-            #print rdz.url
-            #if '.jpg' in rdz.url:
-            #    img(rdz.urlz)
-            #else:
-            #    a(rdz.urlz)
-            #h1(str(rdz.author))
-            
-            #li(img(i.lower(), src='%s' % i))
-
-    with div():
-        attr(cls='body')
-        p('Gify is open source')
-        a('https://github.com/getsdrawn/getsdrawndotcom')
-        a('https://reddit.com/r/redditgetsdrawn')
-
-#print doc
-
-
-# In[53]:
-
-savht = open('/home/' + getusr +'/gify/' + keyword + '/index.html' , 'w')
-
-
-# In[54]:
-
-savht.write(str(doc))
-
-
-# In[55]:
-
-savht.close()
 
